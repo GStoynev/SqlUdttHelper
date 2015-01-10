d2b0c9a31335760c5d1a00a3dbc80167f29612cd
gstoynev <gstoynev@qsystematic.com>
2015-01-10 01:46:59 -0500

    Initial commit

---
+++ SqlUdttHelper/Attributes/DbUdttColumnAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlUdttHelper
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true, Inherited = false)]
+    public class DbUdttColumnAttribute : Attribute
+    {
+        public string UDTTName { get; private set; }
+        public string Name { get; private set; }
+        public int OrdinalPosition { get; private set; }
+        public System.Data.SqlDbType SqlType { get; private set; }
+
+        private DbUdttColumnAttribute() { }
+
+        /// <summary>
+        /// Defines how the decorated field should map to UDTT specified by the <paramref name="Name"/> parameter
+        /// </summary>
+        /// <param name="UDTTName"></param>
+        /// <param name="Name"></param>
+        /// <param name="OrdinalPosition"></param>
+        /// <param name="SqlType"></param>
+        public DbUdttColumnAttribute(string UDTTName, string Name, int OrdinalPosition, System.Data.SqlDbType SqlType)
+        {
+            this.UDTTName = UDTTName;
+            this.Name = Name;
+            this.OrdinalPosition = OrdinalPosition;
+            this.SqlType = SqlType;
+        }
+
+        // this is so that multiple attributes can be used on same target
+        private object _typeId = new object();
+        public override object TypeId
+        {
+            get
+            {
+                return this._typeId;
+            }
+        }
+    }
+}
---
+++ SqlUdttHelperTests/Helpers.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlUdttHelperTests
+{
+    public static class AssertExtensions
+    {
+        public static void Throws(Action func)
+        {
+            var exceptionThrown = false;
+            string exTypeName = String.Empty;
+            try
+            {
+                func.Invoke();
+            }
+            catch (Exception ex)
+            {
+                exceptionThrown = true;
+                exTypeName = ex.GetType().Name;
+            }
+
+            if (!exceptionThrown)
+            {
+                throw new Microsoft.VisualStudio.TestTools.UnitTesting.AssertFailedException(
+                    String.Format("An exception of type {0} was expected, but not thrown", exTypeName)
+                    );
+            }
+        }
+
+        public static void Throws<T>(Action func) where T : Exception
+        {
+            var exceptionThrown = false;
+            try
+            {
+                func.Invoke();
+            }
+            catch (T)
+            {
+                exceptionThrown = true;
+            }
+
+            if (!exceptionThrown)
+            {
+                throw new Microsoft.VisualStudio.TestTools.UnitTesting.AssertFailedException(
+                    String.Format("An exception of type {0} was expected, but not thrown", typeof(T))
+                    );
+            }
+        }
+
+        public static void DoesNotThrow(Action func)
+        {
+            var exceptionThrown = false;
+            string exTypeName = String.Empty;
+            try
+            {
+                func.Invoke();
+            }
+            catch (Exception ex)
+            {
+                exceptionThrown = true;
+                exTypeName = ex.GetType().Name;
+            }
+
+            if (!exceptionThrown)
+            {
+                throw new Microsoft.VisualStudio.TestTools.UnitTesting.AssertFailedException(
+                    String.Format("An exception of type {0} was expected, but not thrown", exTypeName)
+                    );
+            }
+        }
+
+        public static void DoesNotThrow<T>(Action func) where T : Exception
+        {
+            var exceptionThrown = false;
+            try
+            {
+                func.Invoke();
+            }
+            catch (T)
+            {
+                exceptionThrown = true;
+            }
+
+            if (exceptionThrown)
+            {
+                throw new Microsoft.VisualStudio.TestTools.UnitTesting.AssertFailedException(
+                    String.Format("An exception of type {0} was unexpectedly thrown", typeof(T))
+                    );
+            }
+        }
+    }
+}
